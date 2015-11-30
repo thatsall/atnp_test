25924c5f578d07a081b14e95aa4f4377869cfbd2
thatsall <thatsall@naver.com>
2015-12-01 00:09:04 +0900

    test_01
    dd

--- client/views/layouts/main.js
+++ client/views/layouts/main.js
@@ -3,9 +3,9 @@ Template.mainLayout.rendered = function(){
     // Minimalize menu when screen is less than 768px
     $(window).bind("resize load", function () {
         if ($(this).width() < 769) {
-            $('body').addClass('body-small')
+            $('body').addClass('body-small');
         } else {
-            $('body').removeClass('body-small')
+            $('body').removeClass('body-small');
         }
     });
 
